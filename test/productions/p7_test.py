6cb2d6ad3d20472ba4ba020a1448ed1afba1d395
Michal Chrzanowski <mch980821@gmail.com>
2022-01-19 12:10:55 +0100

    p7 - test

--- test/productions/p7_test.py
+++ test/productions/p7_test.py
@@ -78,54 +78,72 @@ class P7Test(unittest.TestCase):
     def test_complex_graph(self):
         # given
         graph = nx.Graph()
+        graph.graph["max_level"] = 3
         graph.add_nodes_from([
-            (1, {"label": 'E', "color": "blue", "level": 0, "pos": (2, 4)}),
-            (2, {"label": "E", "color": "blue", "level": 0, "pos": (2, 2)}),
-            (3, {"label": "E", "color": "blue", "level": 0, "pos": (2, 0)}),
-            (4, {"label": "E", "color": "blue", "level": 0, "pos": (2, 2)}),
-            (5, {"label": "E", "color": "blue", "level": 0, "pos": (2, 0)}),
-            (6, {"label": "I", "color": "brown", "level": 0, "pos": (0, 3)}),
-            (7, {"label": "I", "color": "brown", "level": 0, "pos": (0, 1)}),
-            (8, {"label": "I", "color": "brown", "level": 0, "pos": (4, 3)}),
-            (9, {"label": "I", "color": "brown", "level": 0, "pos": (4, 1)}),
-            (10, {"label": "I", "color": "brown", "level": 0, "pos": (5, 7)}),
-            (11, {"label": "I", "color": "brown", "level": 0, "pos": (6, 8)}),
-            (12, {"label": "I", "color": "brown", "level": 0, "pos": (7, 9)})
+            (0, {"label": "E", "level": 1, "color": "green", "pos": (10, 10)}),
+            (1, {"label": "i", "level": 2, "color": "brown", "pos": (9, 9)}),
+            (2, {"label": "i", "level": 2, "color": "brown", "pos": (11, 9)}),
+            (3, {"label": "I", "level": 3, "color": "grey", "pos": (8, 6)}),
+            (4, {"label": "I", "level": 3, "color": "grey", "pos": (12, 6)}),
+            (5, {"label": "I", "level": 3, "color": "grey", "pos": (12, 4)}),
+            (6, {"label": "I", "level": 3, "color": "grey", "pos": (8, 4)}),
+            (7, {"label": "E", "level": 3, "color": "blue", "pos": (10, 7)}),
+            (8, {"label": "E", "level": 3, "color": "blue", "pos": (10, 7)}),
+            (9, {"label": "E", "level": 3, "color": "blue", "pos": (10, 5)}),
+            (10, {"label": "E", "level": 3, "color": "blue", "pos": (10, 5)}),
+            (11, {"label": "E", "level": 3, "color": "blue", "pos": (10, 3)}),
+            (12, {"label": "E", "level": 3, "color": "blue", "pos": (10, 3)}),
+            (13, {"label": "E", "level": 3, "color": "blue", "pos": (2, 2)}),
+            (14, {"label": "E", "level": 3, "color": "blue", "pos": (14, 9)})
         ])
-        graph.add_edges_from([
-            (1, 2), (1, 4), (1, 6), (1, 8), (1, 11),
-            (2, 3), (2, 6), (2, 7), (2, 12),
-            (3, 7),
-            (4, 5), (4, 8), (4, 9),
-            (5, 9), (5, 10),
 
+        graph.add_edges_from([
+            (0, 1), (0, 2), (1, 3),
+            (1, 6),
+            (2, 4), (2, 5),
+            (3, 7), (3, 9),
+            (6, 9), (6, 11),
+            (4, 8), (4, 10),
+            (5, 10), (5, 12),
+            (7, 9), (9, 11),
+            (8, 10), (10, 12),
+            (13, 6), (14, 5)
         ])
 
         result_graph = nx.Graph()
         result_graph.add_nodes_from([
-            (1, {"label": 'E', "color": "blue", "level": 0, "pos": (2, 4)}),
-            (2, {"label": "E", "color": "blue", "level": 0, "pos": (2, 2)}),
-            (3, {"label": "E", "color": "blue", "level": 0, "pos": (2, 0)}),
-            (6, {"label": "I", "color": "brown", "level": 0, "pos": (0, 3)}),
-            (7, {"label": "I", "color": "brown", "level": 0, "pos": (0, 1)}),
-            (8, {"label": "I", "color": "brown", "level": 0, "pos": (4, 3)}),
-            (9, {"label": "I", "color": "brown", "level": 0, "pos": (4, 1)}),
-            (10, {"label": "I", "color": "brown", "level": 0, "pos": (5, 7)}),
-            (11, {"label": "I", "color": "brown", "level": 0, "pos": (6, 8)}),
-            (12, {"label": "I", "color": "brown", "level": 0, "pos": (7, 9)})
+            (0, {"label": "E", "level": 1, "color": "green", "pos": (10, 10)}),
+            (1, {"label": "i", "level": 2, "color": "brown", "pos": (9, 9)}),
+            (2, {"label": "i", "level": 2, "color": "brown", "pos": (11, 9)}),
+            (3, {"label": "I", "level": 3, "color": "grey", "pos": (8, 6)}),
+            (4, {"label": "I", "level": 3, "color": "grey", "pos": (12, 6)}),
+            (5, {"label": "I", "level": 3, "color": "grey", "pos": (12, 4)}),
+            (6, {"label": "I", "level": 3, "color": "grey", "pos": (8, 4)}),
+            (7, {"label": "E", "level": 3, "color": "blue", "pos": (10, 7)}),
+            (9, {"label": "E", "level": 3, "color": "blue", "pos": (10, 5)}),
+            (11, {"label": "E", "level": 3, "color": "blue", "pos": (10, 3)}),
+            (13, {"label": "E", "level": 3, "color": "blue", "pos": (2, 2)}),
+            (14, {"label": "E", "level": 3, "color": "blue", "pos": (14, 9)})
         ])
+
         result_graph.add_edges_from([
-            (1, 2), (1, 6), (1, 8), (1, 11),
-            (2, 3), (2, 6), (2, 7), (2, 8), (2, 9), (2, 12),
-            (3, 7), (3, 9), (3, 10),
+            (0, 1), (0, 2), (1, 3),
+            (1, 6),
+            (2, 4), (2, 5),
+            (3, 7), (3, 9),
+            (6, 9), (6, 11),
+            (4, 7), (4, 9),
+            (5, 9), (5, 11),
+            (7, 9), (9, 11),
+            (13, 6), (14, 5)
         ])
 
         # when
-        p8(graph)
+        p7(graph, super_simple_strategy(graph, generate_p7_left_side_graph()))
 
         # then
-        self.assertEqual(len(graph.nodes()), 10)
-        self.assertEqual(len(graph.edges()), 13)
+        self.assertEqual(len(graph.nodes()), 12)
+        self.assertEqual(len(graph.edges()), 18)
         self.assertTrue(nx.is_isomorphic(graph, result_graph))
 
     def test_different_position(self):
