6e6390318b9869d6c5c5e816eed9bfdc06031ecd
Damian Sobolewski <damiansobolewski12@gmail.com>
2022-01-23 20:44:13 +0100

    add c production

--- main_c.py
+++ main_c.py
@@ -9,18 +9,10 @@ def main():
     graph = initialize()
     p1(graph)
 
-    i = 0
-    iterations = 2190
-    for _ in range(4):
+    for _ in range(3):
         left_side = generate_c_left_side_graph()
         for m in super_simple_strategy_returning_list(graph, left_side):
             c(graph, m)
-            i += 1
-            if i == iterations:
-                break
-        if i == iterations:
-            break
-    print(i)
 
     # visualize(graph)
     visualize(graph, level=7)
